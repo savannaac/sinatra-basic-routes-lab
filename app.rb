b48debfa47ed18b84b65ae8cf93ddd166e0894ff
savanna chiang <savannaac@gmail.com>
2021-03-15 20:32:43 -0500

    sinatra basic routes lab

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+    # get('/medicines') { some code }  
+    get ("/name") { "My name is Sav" }
+    get ("/hometown") { "My hometown is Valparaiso" }
+    get ("/favorite-song") { "My favorite song is 'More than Friends'" }
+ end
